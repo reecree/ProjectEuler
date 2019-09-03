7c2ff9278fc18e014646aa40c440a8cce7eedd75
chris <ruehschr@gmail.com>
2019-09-02 22:42:20 -0700

    Use const instead of hard coded val

--- p59/main.go
+++ p59/main.go
@@ -7,6 +7,9 @@ import (
 	"strings"
 )
 
+// We're given encryption key is only 3 chars long
+const EncryptionKeyLen = 3
+
 func main() {
 	dat, err := ioutil.ReadFile("./p059_cipher.txt")
 	if err != nil {
@@ -16,13 +19,11 @@ func main() {
 
 	encryptedChars := strings.Split(string(dat), ",")
 
-	// We're given encryption key is only 3 chars long
-	encryptionKeyLen := 3
 	spaceASCIICode := 32
-	charCountMaps := make([]map[int]int, encryptionKeyLen)
-	mostCommonKey := make([]int, encryptionKeyLen)
-	mostCommonVal := make([]int, encryptionKeyLen)
-	for i := 0; i < encryptionKeyLen; i++ {
+	charCountMaps := make([]map[int]int, EncryptionKeyLen)
+	mostCommonKey := make([]int, EncryptionKeyLen)
+	mostCommonVal := make([]int, EncryptionKeyLen)
+	for i := 0; i < EncryptionKeyLen; i++ {
 		charCountMaps[i] = make(map[int]int)
 	}
 	for i, encryptedChar := range encryptedChars {
@@ -31,14 +32,15 @@ func main() {
 			fmt.Print(err.Error())
 			return
 		}
-		charCountMaps[i%3][encryptedInt]++
-		if charCountMaps[i%3][encryptedInt] > mostCommonVal[i%3] {
-			mostCommonVal[i%3] = charCountMaps[i%3][encryptedInt]
-			mostCommonKey[i%3] = encryptedInt
+		ind := i % EncryptionKeyLen
+		charCountMaps[ind][encryptedInt]++
+		if charCountMaps[ind][encryptedInt] > mostCommonVal[ind] {
+			mostCommonVal[ind] = charCountMaps[ind][encryptedInt]
+			mostCommonKey[ind] = encryptedInt
 		}
 	}
 
-	decryptors := make([]int, encryptionKeyLen)
+	decryptors := make([]int, EncryptionKeyLen)
 	for i, key := range mostCommonKey {
 		decryptors[i] = key ^ spaceASCIICode
 	}
@@ -46,7 +48,7 @@ func main() {
 	var sum int
 	for i, encryptedChar := range encryptedChars {
 		encryptedInt, _ := strconv.Atoi(encryptedChar)
-		asciiVal := decryptors[i%3] ^ encryptedInt
+		asciiVal := decryptors[i%EncryptionKeyLen] ^ encryptedInt
 
 		// Print message
 		// fmt.Print(string(asciiVal))
