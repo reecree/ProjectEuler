a9fb1b5ae9deea0c750f5cf5862c02915a800657
chris <ruehschr@gmail.com>
2019-09-04 13:19:23 -0700

    Fixes tests

--- p60/main_test.go
+++ p60/main_test.go
@@ -4,16 +4,16 @@ import "testing"
 
 func TestPrimalityComboCheck(t *testing.T) {
 	sieve, _ := sieveOfEratosthenes(8000, 8000)
-	if !checkPrimalityOfPrimes(7, 3, sieve) {
+	if !checkPrimalityOfCombo(7, 3, sieve) {
 		t.Error("7 and 3 should produce primes when combined")
 	}
-	if !checkPrimalityOfPrimes(7, 109, sieve) {
+	if !checkPrimalityOfCombo(7, 109, sieve) {
 		t.Error("7 and 3 should produce primes when combined")
 	}
-	if checkPrimalityOfPrimes(13, 5, sieve) {
+	if checkPrimalityOfCombo(13, 5, sieve) {
 		t.Error("13 and 5 should not produce primes when combined")
 	}
-	if checkPrimalityOfPrimes(5, 13, sieve) {
+	if checkPrimalityOfCombo(5, 13, sieve) {
 		t.Error("13 and 5 should not produce primes when combined")
 	}
 }
